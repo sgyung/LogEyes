52fd8f6b276f5b83381ea7e0a5c097c80d503fea
sgyung <tjsrud228@naver.com>
2026-01-19 17:27:26 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/logeyes/logdetector/LogdetectorApplication.java
@@ -0,0 +1,13 @@
+package com.logeyes.logdetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogdetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogdetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logeyes/logdetector/LogdetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logeyes.logdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogdetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
